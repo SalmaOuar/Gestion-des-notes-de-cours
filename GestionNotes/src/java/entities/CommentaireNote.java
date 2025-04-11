2417a34b01a5db8298181fee3bdc944caf8c430f
SalmaOuar <ouarsalma34@gmail.com>
2025-04-11 18:35:59 +0100

    Update CommentaireNote.java

--- GestionNotes/src/java/entities/CommentaireNote.java
+++ GestionNotes/src/java/entities/CommentaireNote.java
@@ -1,68 +1,15 @@
-package entities;
+package dao;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import entities.CommentaireNote;
 
-@Entity
-public class CommentaireNote {
+/**
+ *
+ * @author Yoga
+ */
+public class CommentaireNoteDao extends AbstractDao<CommentaireNote> {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    private String contenu;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @ManyToOne
-    @JoinColumn(name = "note_id")
-    private NoteCours note;
-
-    public CommentaireNote() {
-    }
-
-    public CommentaireNote(String contenu, User user, NoteCours note) {
-        this.contenu = contenu;
-        this.user = user;
-        this.note = note;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getContenu() {
-        return contenu;
-    }
-
-    public void setContenu(String contenu) {
-        this.contenu = contenu;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public NoteCours getNote() {
-        return note;
-    }
-
-    public void setNote(NoteCours note) {
-        this.note = note;
+    public CommentaireNoteDao() {
+        super(CommentaireNote.class);
     }
 
 }
